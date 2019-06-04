92dc68f85fce98ee0e75d3c422c0329f67e3af70
vicg39 <vagarciamontiel@gmail.com>
2019-06-04 16:09:50 -0400

    build structure for account components

---
+++ src/components/account/purchaseHistory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PurchaseHistory extends Component {
+    render() {
+        return (
+            <div>
+                purhase info 
+            </div>
+        )
+    }
+}
+
+export default PurchaseHistory;
--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -5,24 +5,8 @@ import {
 } from '../actions/types';
 
 const INITIAL_STATE = {
-    headerLinks: [
-        {
-            title: 'Shop'
-        },
-        {
-            title: 'Logout'
-        }
-    ],
-    navbarLinks: [
-        {
-            title: 'Purchase History',
-            active: true
-        },
-        {
-            title: 'Account Information',
-            active: false
-        }
-    ]
+    headerLinks: [],
+    navbarLinks: []
     
 }
 
