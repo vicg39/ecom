bd269421ba2e77ea616a76945e5a17ad5b33ff9a
vicg39 <vagarciamontiel@gmail.com>
2019-06-05 10:06:05 -0400

    created shopreducer and user reducer

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -6,8 +6,8 @@ import {
 
 const INITIAL_STATE = {
     headerLinks: [],
-    navbarLinks: []
-    
+    navbarLinks: [],
+    onClick: ''
 }
 
 export default function(state = INITIAL_STATE, action) {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
-import { reducer as form} from 'redux-form'
+import { reducer as form } from 'redux-form'; 
 
-import headerNavbar from './headernavbarReducer'
-
+import headerNavbar from './headernavbarReducer';
+import user from './userReducer';
+import shop from './shopReducer';
 
 const rootReducer = combineReducers({
   form,
-  headerNavbar
+  headerNavbar,
+  user,
+  shop
 });
 
 export default rootReducer;
